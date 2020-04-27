826d151f00cf8da91d7744fab73f6e80447623dc
fatlard1993 <fatlard1993>
2020-04-27 16:51:23 -0700

    reballence

--- src/main/java/justfatlard/emerald_armor/EmeraldArmorMaterial.java
+++ src/main/java/justfatlard/emerald_armor/EmeraldArmorMaterial.java
@@ -9,41 +9,41 @@ import net.minecraft.sound.SoundEvents;
 
 public class EmeraldArmorMaterial implements ArmorMaterial {
 	private final static int[] BASE_DURABILITY = new int[]{6, 7, 8, 5};
-	private final static int[] PROTECTION_AMOUNTS = new int[]{3, 6, 8, 3};
-	private final int durabilityMultiplier = 68;
+	private final static int[] PROTECTION_AMOUNTS = new int[]{4, 7, 9, 2};
+	private final int durabilityMultiplier = 72;
 
 	@Override
-	public int getDurability(EquipmentSlot slot) {
+	public int getDurability(EquipmentSlot slot){
 		return BASE_DURABILITY[slot.getEntitySlotId()] * durabilityMultiplier;
 	}
 
 	@Override
-	public Ingredient getRepairIngredient() {
+	public Ingredient getRepairIngredient(){
 		return Ingredient.ofItems(Items.EMERALD_BLOCK);
 	}
 
 	@Override
-	public int getEnchantability() {
+	public int getEnchantability(){
 		return 22;
 	}
 
 	@Override
-	public int getProtectionAmount(EquipmentSlot slot) {
+	public int getProtectionAmount(EquipmentSlot slot){
 		return PROTECTION_AMOUNTS[slot.getEntitySlotId()];
 	}
 
 	@Override
-	public String getName() {
+	public String getName(){
 		return "emerald";
 	}
 
 	@Override
-	public SoundEvent getEquipSound() {
+	public SoundEvent getEquipSound(){
 		return SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND;
 	}
 
 	@Override
-	public float getToughness() {
+	public float getToughness(){
 		return 1;
 	}
 }
